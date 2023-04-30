ce1af13f20bb049d744f1af5340ad6dde8c028b9
Rizqi Pratama <43211197+skipfortoday@users.noreply.github.com>
2023-04-30 13:42:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           max_tokens=256,
           top_p=1,
           frequency_penalty=0,
-          presence_penalty=0
+          presence_penalty=0,
         });
         setPrompt("");
         setChats([
