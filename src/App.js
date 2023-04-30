35a26797a5930a909265e6a376f80318ba8ea547
Rizqi Pratama <43211197+skipfortoday@users.noreply.github.com>
2023-04-30 13:39:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,8 +18,11 @@ function App() {
         const response = await openai.createCompletion({
           model: "text-davinci-003",
           prompt: prompt,
-          temperature: 0.5,
-          max_tokens: 100,
+          temperature=0.7,
+          max_tokens=256,
+          top_p=1,
+          frequency_penalty=0,
+          presence_penalty=0
         });
         setPrompt("");
         setChats([
